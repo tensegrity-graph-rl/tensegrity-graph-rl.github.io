7eb5c8accbeeb20b41d505a52a1db6d3b0c48731
Chi Zhang <zhc@umich.edu>
2026-04-29 11:57:54 -0400

    [dev] replace teasor video; carousel autoplay speed: 5s -> 10s

--- static/js/index.js
+++ static/js/index.js
@@ -128,7 +128,7 @@ $(document).ready(function() {
 		loop: true,
 		infinite: true,
 		autoplay: true,
-		autoplaySpeed: 5000,
+		autoplaySpeed: 10000,
     }
 
 	// Initialize all div with carousel class
